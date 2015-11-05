4a9d9ef2f201777d1a47185ad01dbcd40d0bdc60
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-05 05:46:12 -0700

    rockin it

--- scripts/quickreduce.py
+++ scripts/quickreduce.py
@@ -11,113 +11,176 @@ from collections import defaultdict
 import os
 
 # Third-party
-from astropy.io import fits
+import ccdproc
 from astropy import log as logger
+import astropy.units as u
 import matplotlib.pyplot as pl
+from matplotlib.widgets import Cursor
 import numpy as np
 import scipy.optimize as so
-from scipy.stats import scoreatpercentile
+from astropy.modeling import models
+from scipy.signal import medfilt2d
+
+# suppress warning about correlated errors
+from astropy import nddata
+nddata.conf.warn_unsupported_correlated = False
+
+# Properties of Echelle at MDM
+gain = 1.3 * u.electron / u.adu
+readnoise = 7.9 * u.electron
 
 def gaussian_constant(pix, A, mu, sigma, B):
     return A/(np.sqrt(2*np.pi)*sigma) * np.exp(-0.5 * ((pix - mu)/sigma)**2) + B
 
-def min_func(p, pix, intensity):
-    A,mu,sigma,B = p
+def oscan_and_trim(image_list, overscan_idx):
+    """
+    Subtract overscan and trim out the overscan region for each image
+    in a list of images. The original list is modified in place.
+    """
+    for idx, img in enumerate(image_list):
+        oscan = ccdproc.subtract_overscan(img, img[:, overscan_idx+8:overscan_idx+72], model=models.Polynomial1D(1), add_keyword=None)
+        image_list[idx] = ccdproc.trim_image(oscan[:, :overscan_idx], add_keyword=None)
 
-    if A <= 0. or sigma <= 0. or B <= 0 or mu < pix.min() or mu > pix.max():
-        return 1E10
+def auto_scale_imshow(d, nsigma=5, **kwargs):
+    _mean = np.mean(d)
+    _std = np.std(d)
 
-    model = gaussian_constant(pix, A, mu, sigma, B)
-    return np.sum((model - intensity)**2)
+    fig,ax = pl.subplots(1,1,**kwargs)
+    ax.imshow(d, cmap='Greys_r', interpolation='nearest',
+              vmax=_mean + 5*_std,
+              vmin=_mean - 5*_std)
+    return fig
 
-def main(datafile, flatfile, biasfile, overscan_idx,
-         dispersion_stride=1):
+def make_master_bias(images, oscan_idx, plot=False):
+    """ Create a master bias file from the image collection """
+    bias_list = []
+    for hdu, fname in images.hdus(imagetyp='bias', return_fname=True):
+        bias_list.append(ccdproc.CCDData(hdu.data*u.adu))
+    logger.info("Using {} bias frames.".format(len(bias_list)))
+
+    oscan_and_trim(bias_list, oscan_idx)
+
+    # median combine the bias images
+    biases = ccdproc.Combiner(bias_list)
+    master_bias = biases.median_combine()
+
+    if plot:
+        fig = auto_scale_imshow(master_bias.data, figsize=(10,10))
+
+    return master_bias
+
+def make_master_flat(images, master_bias, oscan_idx, plot=False):
+    """ Create a master flatfield from the image collection """
+    flat_list = []
+    for hdu, fname in images.hdus(imagetyp='flat', return_fname=True):
+        flat_list.append(ccdproc.CCDData(hdu.data*u.adu))
+    logger.info("Using {} flat frames.".format(len(flat_list)))
+
+    oscan_and_trim(flat_list, oscan_idx)
+
+    # bias correct the flats
+    for i,flat in enumerate(flat_list):
+        flat_list[i] = ccdproc.subtract_bias(flat, master_bias, add_keyword=None)
+
+    # combine the flats using sigma clipping to reject cosmic rays (?)
+    flat_combiner = ccdproc.Combiner(flat_list)
+    flat_combiner.sigma_clipping(func=lambda x,axis: np.nanmedian(x,axis))
+    master_flat = flat_combiner.median_combine()
+
+    # correct for gain in CCD
+    master_flat_e = ccdproc.gain_correct(master_flat, gain=gain, add_keyword=None)
+
+    if plot:
+        fig = auto_scale_imshow(master_flat_e.data, figsize=(10,10))
+
+    return master_flat_e
+
+def main(path, datafile, oscan_idx, ntracebins=32, median_filter_kernel=3, interactive=False):
     """ """
 
-    SIGMAINIT = 1. # pixels, for fitting Gaussian to row
+    # all FITS images
+    images = ccdproc.ImageFileCollection(path, keywords='*')
+    master_bias = make_master_bias(images, oscan_idx)
+    master_flat = make_master_flat(images, master_bias, oscan_idx)
 
-    # read data from files
-    data = fits.getdata(os.path.abspath(os.path.expanduser(datafile)), 0)
-    flat = fits.getdata(os.path.abspath(os.path.expanduser(flatfile)), 0)
-    bias = fits.getdata(os.path.abspath(os.path.expanduser(biasfile)), 0)
+    # read the data frame
+    obj_img = ccdproc.CCDData.read(os.path.abspath(os.path.expanduser(datafile)),
+                                   unit=u.adu)
+    obj_img = [obj_img] # HACK
+    oscan_and_trim(obj_img, oscan_idx) # HACK
+    obj_img = obj_img[0] # HACK
 
-    with np.errstate(divide='ignore'):
-        # C = (data - bias) / (flat - bias)
-        C = data / flat
+    obj_img = ccdproc.subtract_bias(obj_img, master_bias, add_keyword=None)
+    obj_img = ccdproc.flat_correct(obj_img, master_flat, add_keyword=None)
 
-    tmp = defaultdict(lambda: None)
-    def on_key_press(event, fig):
-        if event.key == 'c':
-            tmp['dispersion_axis'] = 0
-            tmp['spectrum_idx'] = event.xdata
-            pl.close(fig)
+    if median_filter_kernel > 1:
+        d = medfilt2d(obj_img.data, kernel_size=(median_filter_kernel,median_filter_kernel))
+    else:
+        d = obj_img.data
 
-        elif event.key == 'l' or event.key == 'r':
-            tmp['dispersion_axis'] = 1
-            tmp['spectrum_idx'] = event.ydata
-            pl.close(fig)
+    if interactive:
+        tmp = defaultdict(lambda: None)
+        def on_key_press(event, fig):
+            if event.key == 'c':
+                tmp['dispersion_axis'] = 0
+                tmp['spectrum_idx'] = event.xdata
+                pl.close(fig)
 
-    # TODO: make this less hacky
-    # vmin = -0.1
-    # vmax = 0.2
-    vmin = 0.5
-    vmax = 1.5
+            elif event.key == 'l' or event.key == 'r':
+                tmp['dispersion_axis'] = 1
+                tmp['spectrum_idx'] = event.ydata
+                pl.close(fig)
 
-    fig,ax = pl.subplots(1,1)
-    ax.imshow(C, cmap='Greys_r', vmin=vmin, vmax=vmax,
-              interpolation='nearest', origin='lower')
-    fig.canvas.mpl_connect('key_press_event', lambda event: on_key_press(event, fig))
-    fig.tight_layout()
-    pl.show()
+        # show image of the CCD, let user set approximate position of the spectrum
+        #   to extract
+        fig = auto_scale_imshow(d, figsize=(10,10))
+        cursor = Cursor(fig.axes[0], useblit=False, horizOn=False,
+                        color='red', linewidth=1)
+        fig.canvas.mpl_connect('key_press_event', lambda event: on_key_press(event, fig))
+        fig.tight_layout()
+        pl.show()
 
-    dispersion_axis = tmp['dispersion_axis']
-    spectrum_idx = tmp['spectrum_idx']
+        dispersion_axis = tmp['dispersion_axis']
+        spectrum_idx = tmp['spectrum_idx']
 
-    if dispersion_axis is None:
-        raise ValueError("Did you close the plot without selecting a row "
-                         "or column to extract along?")
+        if dispersion_axis is None:
+            raise ValueError("Did you close the plot without selecting a row "
+                             "or column to extract along?")
 
-    C_swap = C.swapaxes(dispersion_axis, 0)
+    else:
+        _idx = np.linspace(0, d.shape[0]-1, min(32,d.shape[0])).astype(int)
 
-    # slice out overscan region
-    overscan = C_swap[:,overscan_idx:]
-    C_swap = C_swap[:,:overscan_idx]
+        dispersion_axis = np.argmax(d.shape)
+        spectrum_idx = np.median(d[_idx].argmax(axis=-1))
 
     # pixels down dispersion axis
-    dispersion_px = np.arange(0, C_swap.shape[0], dispersion_stride, dtype=int)
-    amps = np.zeros_like(dispersion_px).astype(float)
-    locs = np.zeros_like(dispersion_px).astype(float)
+    dispersion_px = np.arange(d.shape[0], dtype=float)
+    slit_px = np.arange(d.shape[1], dtype=float)
 
-    # pixels across slit
-    row_px = np.arange(C_swap.shape[1])
+    trace_bin_idx = np.linspace(0, d.shape[0], ntracebins).astype(int)
+    flux = np.zeros_like(dispersion_px)
+    for i1,i2 in zip(trace_bin_idx[:-1],trace_bin_idx[1:]):
+        row = np.median(d[i1:i2], axis=0)
 
-    for j,i in enumerate(dispersion_px):
-        row = C_swap[i]
         p0 = (row[int(round(spectrum_idx))],
               spectrum_idx,
-              SIGMAINIT,
+              2., # initial spread
               np.median(row))
+        try:
+            p,_ = so.curve_fit(gaussian_constant, slit_px, row, p0=p0)
+        except RuntimeError:
+            logger.debug("Failed to fit Gaussian.")
+            continue
 
-        res = so.minimize(min_func, x0=p0, method='powell',
-                          args=(row_px, row))
-        amps[j] = res.x[0]
-        locs[j] = res.x[1]
+        if p[0] < 0.:
+            continue
 
-        # if i == 1360 or i == 1372:
-        #     print(res.x)
-        #     pl.clf()
-        #     pl.plot(row_px, row, marker=None, drawstyle='steps', lw=1.)
-        #     pl.plot(row_px, gaussian_constant(row_px, *res.x), marker=None, drawstyle='steps', lw=1.)
-        #     pl.axvline(res.x[1])
-        #     pl.axvline(p0[1], linestyle='dashed')
-        #     pl.show()
-
-    # fig,ax = pl.subplots(1,1)
-    # ax.plot(locs, marker=None, drawstyle='steps')
-    # pl.show()
+        left = int(round(p[1] - 3*p[2]))
+        right = int(round(p[1] + 3*p[2]))
+        flux[i1:i2] = d[i1:i2,left:right].sum(axis=1)
 
     fig,ax = pl.subplots(1,1)
-    ax.plot(dispersion_px, amps, marker=None, drawstyle='steps')
+    ax.plot(flux, marker=None, drawstyle='steps')
     pl.show()
 
 if __name__ == "__main__":
@@ -130,18 +193,20 @@ if __name__ == "__main__":
                         default=False, help="Be chatty! (default = False)")
     parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
                         default=False, help="Be quiet! (default = False)")
+    parser.add_argument("-i", "--interactive", action="store_true", dest="interactive",
+                        default=False, help="Interactive mode!")
 
     parser.add_argument("--data", dest="datafile", required=True,
                         type=str, help="Data frame.")
-    parser.add_argument("--flat", dest="flatfile", required=True,
-                        type=str, help="Flatfield image.")
-    parser.add_argument("--bias", dest="biasfile", required=True,
-                        type=str, help="Bias image.")
-    parser.add_argument("--overscan-idx", dest="overscan", required=True,
+    parser.add_argument("--path", dest="path", required=True,
+                        type=str, help="Path to flats and bias files.")
+    parser.add_argument("--oscan-idx", dest="overscan", required=True,
                         type=int, help="Index of the overscan region.")
-
-    parser.add_argument("--stride", dest="stride", default=1,
-                        type=int, help="Skip every X row on the CCD.")
+    parser.add_argument("--filter-kernel-size", dest="ksize", default=1,
+                        type=int, help="Size of the median filter kernel (in pixels).")
+    parser.add_argument("--ntracebins", dest="ntracebins", default=32,
+                        type=int, help="Number of bins to use for finding the trace of the "
+                                       "spectrum along the dispersion axis.")
 
     args = parser.parse_args()
 
@@ -153,5 +218,5 @@ if __name__ == "__main__":
     else:
         logger.setLevel(logging.INFO)
 
-    main(datafile=args.datafile, flatfile=args.flatfile, biasfile=args.biasfile,
-         overscan_idx=args.overscan, dispersion_stride=args.stride)
+    main(path=args.path, datafile=args.datafile, oscan_idx=args.overscan, ntracebins=args.ntracebins,
+         interactive=args.interactive, median_filter_kernel=args.ksize)
