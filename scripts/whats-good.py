83da9d87f3f20d1cfd1b8fc1fa8ed4291dafb447
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-06 00:46:28 -0700

    date and time spec

--- scripts/whats-good.py
+++ scripts/whats-good.py
@@ -33,13 +33,29 @@ def Vmag_to_exptime(V):
     y = 10**s(V)
     return y
 
-def main(filename):
+# TODO: flag to hide completed stars
+# TODO: flag to specify date/time
+
+def main(filename, donefile=None, ut_date=None, ut_time=None):
     filepath = os.path.abspath(os.path.expanduser(filename))
     tbl = ascii.read(filepath)
+    if 'ID2013' in tbl.colnames:
+        tbl = tbl[tbl['ID2013'] == '--']
+
+    if donefile is not None:
+        # if a file specified, don't show done stars
+        donelist = np.loadtxt(donefile, dtype=str)
+        tbl = tbl[~np.in1d(tbl['ID2015'], donelist)]
+
     ntargets = len(tbl)
 
-    # get current time
-    time = atime.Time(datetime.utcnow(), scale='utc')
+    # get current time and date
+    if ut_date is None:
+        time = atime.Time(datetime.utcnow(), scale='utc')
+    else:
+        yr,mo,da = map(int, ut_date.split("-"))
+        hr,mi = map(int, ut_time.split(":"))
+        time = atime.Time(datetime(yr,mo,da,hour=hr,minute=mi,second=0), scale='utc')
 
     phases = np.zeros(ntargets)
     for i in range(ntargets):
@@ -49,11 +65,16 @@ def main(filename):
         # HACK: this corrects for the fact that CSS defines phase = 0.5
         #   to be peak brightness...Brani uses phase = 0.
         # phases[i] = (phases[i] - 0.5) % 1.
-        # UPDATE: actually, I think CSS uses the peak...
+        # UPDATE: actually, I think CSS does use the peak...
 
-    ix = (phases > 0.05) & (phases < 0.8)
-    good_targets = tbl[ix]
-    good_targets['phase'] = phases[ix]
+    if ut_date is None:
+        ix = (phases > 0.05) & (phases < 0.8)
+        good_targets = tbl[ix]
+        good_targets['phase'] = phases[ix]
+
+    else:
+        good_targets = tbl
+        good_targets['phase'] = phases
 
     # add a column with estimated exposure times
     good_targets['exptime'] = [Vmag_to_exptime(V) for V in good_targets['VmagAvg']]
@@ -86,8 +107,12 @@ if __name__ == "__main__":
 
     parser.add_argument("-f", "--file", dest="file", required=True,
                         type=str, help="Catalog file.")
-    # parser.add_argument("-p", dest="plot", action="store_true", default=False,
-    #                     help="Plot or not")
+    parser.add_argument("--done", dest="donefile", default=None,
+                        help="Don't include stars that are in this list.")
+    parser.add_argument("--utdate", dest="utdate", default=None,
+                        help="UT date as a string, e.g., 2015-11-06.")
+    parser.add_argument("--uttime", dest="uttime", default=None,
+                        help="UT time as a string, e.g., 06:11.")
 
     args = parser.parse_args()
 
@@ -99,4 +124,4 @@ if __name__ == "__main__":
     else:
         logger.setLevel(logging.INFO)
 
-    main(filename=args.file)
+    main(filename=args.file, donefile=args.donefile, ut_date=args.utdate, ut_time=args.uttime)
